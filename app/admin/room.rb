6bb868315c7b9dcad0f4869d418e4fd6a12bf2bf
taketo957 <taketo957@gmail.com>
2014-11-26 14:17:53 +0900

    編集許可の変更

--- app/admin/room.rb
+++ app/admin/room.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Room do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :toilet_id, :washlet
+  permit_params :toilet_id, :washlet, :available, :multipurpose
   #
   # or
   #
--- app/admin/toilet.rb
+++ app/admin/toilet.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Toilet do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :building_id, :store_id, :latitude, :longitude, :multipurpose, :floor, :sex
+  permit_params :building_id, :store_name, :latitude, :longitude, :multipurpose, :floor, :sex
   #
   # or
   #
